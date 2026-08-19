5431443eb64e787dd85f249627f06d7cec78c0e4
Yohannes getachew <108579812+Yohannesgetachewb@users.noreply.github.com>
2026-08-19 13:38:39 -0700

    Implement counting sort algorithm in counting sort.py

---
+++ counting sort.py
@@ -0,0 +1,20 @@
+import os
+
+def countingSort(arr):
+    frequency = [0] * 100
+    for num in arr:
+        frequency[num] += 1
+    return frequency
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
