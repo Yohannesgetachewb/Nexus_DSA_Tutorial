f77a7675cca7d280a03fbec77b3635f1dc1ef6d6
Yohannes getachew <108579812+Yohannesgetachewb@users.noreply.github.com>
2026-08-21 12:11:44 -0700

    Simplify player-trainer matching logic
    Refactor matching logic to simplify trainer iteration.

---
+++ Maximum Matching of Players With Trainers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: list[int], trainers: list[int]) -> int:
+        players.sort()
+        trainers.sort()
+        
+        i, j = 0, 0
+        
+        while i < len(players) and j < len(trainers):
+            if players[i] <= trainers[j]:
+                i += 1  
+            j += 1      
+            
+        return i
